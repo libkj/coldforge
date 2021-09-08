42f68c33102490bb59e0b226fabcea130abdb4d8
Chuck Coleman <chuck.coleman@chuckwagon.dev>
2021-09-08 16:53:07 -0400

    fixed table name typo

--- sql/settings.sql
+++ sql/settings.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `hosts`;
-CREATE TABLE `hosts` (
+DROP TABLE IF EXISTS `allowed_hosts`;
+CREATE TABLE `allowed_hosts` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `host` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
